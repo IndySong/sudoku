ddfc2a6cff9336c92f2b33abd57f7ba27d7aae8b
Raga Markely <lmarkely@alum.mit.edu>
2017-09-18 19:32:21 -0400

    feat: Add function to update sudoku

--- solution.py
+++ solution.py
@@ -133,7 +133,35 @@ def only_choice(values):
     return values
 
 def reduce_puzzle(values):
-    pass
+    """
+    Use Eliminate Strategy and Only Choice Strategy
+
+    Args: Sudoku in dictionary form.
+    Returns: Resulting Sudoku in dictionary form after eliminating invalid
+             choice and filling in only choices.
+    """
+    stalled = False
+    while not stalled:
+        # Check how many boxes have a determined value
+        solved_values_before = len([box for box in values.keys() if
+                                   len(values[box]) == 1])
+
+        # Your code here: Use the Eliminate Strategy
+        values = eliminate(values)
+
+        # Your code here: Use the Only Choice Strategy
+        values = only_choice(values)
+
+        # Check how many boxes have a determined value, to compare
+        solved_values_after = len([box for box in values.keys() if
+                                  len(values[box]) == 1])
+        # If no new values were added, stop the loop.
+        stalled = solved_values_before == solved_values_after
+        # Sanity check, return False if there is a box with zero
+        # available values:
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
 
 def search(values):
     pass
