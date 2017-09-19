133634075103a29fa0552e4ea911a0194b30ef35
Raga Markely <lmarkely@alum.mit.edu>
2017-09-18 20:13:26 -0400

    fix: Fix solution test

--- solution.py
+++ solution.py
@@ -206,8 +206,7 @@ def solve(grid):
     return values if values else False
 
 if __name__ == '__main__':
-    diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...'
-                       '4....8....52.............3'
+    diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
     display(solve(diag_sudoku_grid))
 
     try:
